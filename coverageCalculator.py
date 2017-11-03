05903547730724d9fe3296759dfb7b471a1493d5
Kreiva Laboratories <kreivalabs@gmail.com>
2017-11-02 20:23:18 -0700

    Update to version 1.1
    Add title block. Improve formatting of returned results. Add rounding function to results.

--- coverageCalculator.py
+++ coverageCalculator.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
-#requires Future additions (http://www.python-future.org)
-#Calculate speaker coverage based on driver dispersion angle and measured distance from source.
 
-#import Future functions
+# version 1.1 02-November-2017
+
+# Requires Future additions (http://www.python-future.org)
+# Calculate speaker coverage based on driver dispersion angle and measured distance from source.
+
+# Import Future functions
 from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import input
 import math
 
-#prompt for user input
+title="Loudspeaker Coverage Calculator"
+print(title)
+print("=" * 80)
+
+# Prompt for user input
 cone_degrees = int(input("Enter speaker dispersion angle in degrees: "))
 measured_distance = float(input("Enter measured distance from speaker in feet: "))
 
-#convert angles to radians
+# Convert angles to radians
 angle_radians = math.radians(cone_degrees)
 radians_div = angle_radians / 2
 
-#calculate coverage pattern
+# Calculate coverage pattern
 coverage_pattern = ((math.tan(radians_div)) * measured_distance)* 2
 
-#display results, prompt for user input to quit.
-print('Speaker coverage pattern is: ', coverage_pattern, 'ft.')
-print('Press the <Enter> key to exit.')
+# Round results to two decimal places
+coverage_pattern_round = str(round(coverage_pattern, 2))
+
+# Return results, prompt for user input to quit
+print("Approximate speaker coverage pattern is: ", coverage_pattern_round, "ft.")
+print()
+print("Press the <Enter> key to exit.")
 input()
